1cec15d6a684628ce9a113b44e756cdeaad44864
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-08-17 00:22:15 +1000

    added main.js under scripts

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const { SolQL } = require('solql');
+const hre = require('hardhat');
+const { ethers } = require('ethers');
+
+
+
+
+
+
+async function main() {
+
+    const provider = new ethers.providers.JsonRpcProvider('YOUR PROVIDER URL HERE');
+    const contractName = 'Query';
+    const chainId = '5';
+
+    const solql = new SolQL(contractName, chainId, provider, hre);
+
+    const result = await solql.query.run();
+
+    console.log(result);
+
+    return result;
+
+}
+
+
+
+
+
+main()
+.then()
+.catch(error => {
+    console.error(error);
+    process.exit(1);
+})
