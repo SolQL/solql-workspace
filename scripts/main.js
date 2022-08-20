1df4d29bab4e41892c03e4d418c977d3e4bdb2fc
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-08-19 20:05:42 -0500

    added ethers.js decoder line to decode raw result

--- scripts/main.js
+++ scripts/main.js
@@ -15,9 +15,15 @@ async function main() {
 
     const solql = new SolQL(contractName, chainId, provider, hre);
 
-    const result = await solql.query.run();
+    const result = await solql.query.run(); //this is your raw, bytes encoded result
 
     console.log(result);
+
+    const decodedResult = ethers.utils.defaultAbiCoder.decode(["address[100]"], result); //this decodes the returned result into the appropriate type (type depends on your particular query contract).
+
+    console.log(decodedResult);
+
+
 
     return result;
 
